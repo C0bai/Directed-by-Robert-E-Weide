b73101ec08be49b2fc5ca00935dbe8da7237abc9
Cobai <dbullimore25@gmail.com>
2024-02-14 22:22:16 +0000

    modified:   control.lua modified:   data.lua

--- control.lua
+++ control.lua
@@ -1,4 +1,4 @@
--- Called after a player dies.
+-- Called after a player dies & plays the sound file
 script.on_event(defines.events.on_player_died, function(event)
 		game.play_sound({path = "sfx_robeweide"})
 end)
--- data.lua
+++ data.lua
@@ -1,11 +1,11 @@
 --Replace game_lost sound for singleplayer.
 data.raw["utility-sounds"]["default"]["game_lost"] =
-	{{filename = "__Directed_By_Robert_E_Weide__/sound/directed_by_robert_e_wiede.ogg", volume=1.0}}
+	{{filename = "__Directed_By_Robert_E_Weide__/sound/directed_by_robert_e_wiede.ogg", volume=1.0}} --Location of sound file - must include full mod folder name and sub folders
 
 --Add on_player_died sound for multiplayer.
 data:extend({{
 	type="sound",
 	name="sfx_robeweide",
-	filename="__Directed_By_Robert_E_Weide__/sound/directed_by_robert_e_wiede.ogg",
+	filename="__Directed_By_Robert_E_Weide__/sound/directed_by_robert_e_wiede.ogg", -- Parent Directory must use double underscore before and after
 	volume=1.0
 }})
